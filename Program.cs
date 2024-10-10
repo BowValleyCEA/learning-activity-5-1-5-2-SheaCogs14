227656da4b61e0995f2b0be106ee59b0d54427fc
Shea <s.coughlin037@mybvc.ca>
2024-10-09 18:50:40 -0600

    POS System start
    added the classes for users and videos, added some more input in the menu, still need to add the logic to rent the movie, return it and input for the customers name

--- Program.cs
+++ Program.cs
@@ -1,121 +1,292 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-Console.WriteLine("Hello, World!");
-List<(string Initials, int Score)> highScores = new List<(string, int)>();
-LearningActivity5_1();
-//LearningActivity5_2();
-return;
-
-
-
-
-void LearningActivity5_1()
+class Program
 {
+    static List<(string Initials, int Score)> highScores = new List<(string, int)>();
 
-
-
-    Random randomScore = new Random();
-    while (true)
+    static void Main(string[] args)
     {
-        GameSelection choice = ChooseOption();
+        Console.WriteLine("Hello, World!");
 
-        switch (choice)
+
+
+        // LearningActivity5_1(); 
+        LearningActivity5_2();   
+    }
+
+
+    static void LearningActivity5_1()
+    {
+        Random randomScore = new Random();
+        while (true)
         {
-            case GameSelection.Play:
-                int newHighestScore = randomScore.Next(1000, 1000000);
+            GameSelection choice = ChooseGameOption();
 
-                Console.WriteLine("Please enter your initials (exactly 3 characters):");
+            switch (choice)
+            {
+                case GameSelection.Play:
+                    int newHighestScore = randomScore.Next(1000, 1000000);
+                    Console.WriteLine("Please enter your initials (exactly 3 characters):");
+                    string initials = Console.ReadLine().ToUpper();
 
-                string initials = Console.ReadLine().ToUpper(); 
+                    if (initials.Length != 3)
+                    {
+                        Console.WriteLine("Invalid input. Please use initials limited to 3 characters.");
+                        break;
+                    }
 
-                if (initials.Length != 3)
-                {
-                    Console.WriteLine("Invalid input. Please use initials limited to 3 characters.");
-                    break; 
-                }
+                    AddHighScore(initials, newHighestScore);
+                    Console.WriteLine($"You finished with a score of {newHighestScore}");
+                    break;
+
+                case GameSelection.SeeHighScore:
+                    DisplayHighScores();
+                    break;
+
+                case GameSelection.Exit:
+                    return;
+            }
+        }
+    }
+
+    static void AddHighScore(string initials, int score)
+    {
+        highScores.Add((initials, score));
+        highScores.Sort((a, b) => b.Score.CompareTo(a.Score));
+        if (highScores.Count > 10)
+        {
+            highScores.RemoveAt(10);
+        }
+    }
+
+    static void DisplayHighScores()
+    {
+        Console.WriteLine("Top 10 High Scores:");
+        foreach (var (initials, score) in highScores)
+        {
+            Console.WriteLine($"{initials}: {score}");
+        }
+    }
+
+    static GameSelection ChooseGameOption()
+    {
+        bool validSelection = false;
+        int selection = 0;
+        while (!validSelection)
+        {
+            Console.WriteLine("Would you like to:\n\t 1: Play\n\t 2: See the list of high scores\n\t 3: Exit the game?");
+            if (int.TryParse(Console.ReadLine(), out selection) && selection >= 1 && selection <= 3)
+                validSelection = true;
+        }
+
+        return (GameSelection)selection;
+    }
+
+    enum GameSelection
+    {
+        Play = 1, SeeHighScore = 2, Exit = 3
+    }
+
+    // POS System Logic
+    static void LearningActivity5_2()
+    {
+        VideoRentalStore store = new VideoRentalStore();
+
+        store.AddVideo("The Godfather", "Crime", 175);
+
+        store.AddUser("Shea");
 
 
-                AddHighScore(initials, newHighestScore);
-                Console.WriteLine($"You finished with a score of {newHighestScore}");
-                break;
-            case GameSelection.SeeHighScore:
-                DisplayHighScores();
-                break;
-            case GameSelection.Exit:
-                return;
+        while (true)
+        {
+            VideoSelection choice = ChooseVideoOption();
+
+            switch (choice)
+            {
+                case VideoSelection.Rent:
+                    Console.WriteLine("Please input the customers name");
+                    string userName = Console.ReadLine();
+                    User? user = store.Users.FirstOrDefault(u => u.Name == userName);
+
+                    if (user != null)
+                    {
+                        Console.WriteLine("Below is a list of movies available");
+                        foreach (var video in store.SearchVideos(isAvailable: true))
+                        {
+                            Console.WriteLine($"{video.VideoId}: {video.Title} ({video.Genre}) - {video.Duration} minutes");
+                        }
+                        Console.WriteLine("Please entre the video ID");
+                        
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("No user found!");
+                      
+                    }
+                    break;
+
+                case VideoSelection.List:
+                    Console.WriteLine("List of movies available will display");
+                    break;
+
+                case VideoSelection.Exit:
+                    return;
+            }
+        }
+    }
+
+    static VideoSelection ChooseVideoOption()
+    {
+        bool validSelection = false;
+        int selection = 0;
+        while (!validSelection)
+        {
+            Console.WriteLine("Would you like to:\n\t 1: Rent a Video\n\t 2: See the list of videos for rent\n\t 3: Exit the program?");
+            if (int.TryParse(Console.ReadLine(), out selection) && selection >= 1 && selection <= 3)
+                validSelection = true;
+        }
+
+        return (VideoSelection)selection;
+    }
+
+    enum VideoSelection
+    {
+        Rent = 1, List = 2, Exit = 3
+    }
+}
+
+public class User
+{
+    private static int nextId = 100;
+    public int UserId { get; set; }
+    public string Name { get; set; }
+    public List<Video> CurrentRentals { get; private set; }
+    public List<Video> RentalHistory { get; private set; }
+
+    public User(string name)
+    {
+        UserId = nextId++;
+        Name = name;
+        CurrentRentals = new List<Video>();
+        RentalHistory = new List<Video>();
+    }
+
+    public void RentVideo(Video video)
+    {
+        CurrentRentals.Add(video);
+    }
+
+    public void ReturnVideo(Video video)
+    {
+        CurrentRentals.Remove(video);
+        RentalHistory.Add(video);
+    }
+
+    public override string ToString()
+    {
+        return $"User ID: {UserId}, Name: {Name}";
+    }
+}
+
+public class Video
+{
+    private static int nextId = 1;
+    public int VideoId { get; private set; }
+    public string Title { get; private set; }
+    public string Genre { get; private set; }
+    public int Duration { get; private set; }
+    public bool IsRented { get; private set; }
+    public User? RentedBy { get; private set; } 
+
+    public Video(string title, string genre, int duration)
+    {
+        VideoId = nextId++;
+        Title = title;
+        Genre = genre;
+        Duration = duration;
+        IsRented = false;
+        RentedBy = null;  
+    }
+
+    public void Rent(User user)
+    {
+        IsRented = true;
+        RentedBy = user;
+    }
+
+    public void Return()
+    {
+        IsRented = false;
+        RentedBy = null;  
+    }
+
+    public override string ToString()
+    {
+        return $"Video ID: {VideoId}, Title: {Title}, Genre: {Genre}, Duration: {Duration} mins, Is Rented: {IsRented}";
+    }
+}
+
+public class VideoRentalStore
+{
+    public List<User> Users { get; private set; }
+    public List<Video> Videos { get; private set; }
+
+    public VideoRentalStore()
+    {
+        Users = new List<User>();
+        Videos = new List<Video>();
+    }
+
+    public void AddUser(string name)
+    {
+        Users.Add(new User(name));
+    }
+
+    public void AddVideo(string title, string genre, int duration)
+    {
+        Videos.Add(new Video(title, genre, duration));
+    }
+
+    public List<Video> SearchVideos(bool isAvailable = false)  
+    {
+        if (isAvailable)
+        {
+            return Videos.Where(v => !v.IsRented).ToList(); 
+        }
+        return Videos;
+    }
+
+    public void RentVideo(User user, int videoId)
+    {
+        Video? video = Videos.FirstOrDefault(v => v.VideoId == videoId && !v.IsRented);
+        if (video != null)
+        {
+            video.Rent(user);
+            user.RentVideo(video);
+            Console.WriteLine($"{user.Name} successfully rented {video.Title}.");
+        }
+        else
+        {
+            Console.WriteLine("Video is either not available or does not exist.");
+        }
+    }
+
+    public void ReturnVideo(User user, int videoId)
+    {
+        Video? video = user.CurrentRentals.FirstOrDefault(v => v.VideoId == videoId);
+        if (video != null)
+        {
+            video.Return();
+            user.ReturnVideo(video);
+            Console.WriteLine($"{user.Name} successfully returned {video.Title}.");
+        }
+        else
+        {
+            Console.WriteLine("The user has not rented this video.");
         }
     }
 }
-
-void AddHighScore(string initials, int Score)
-{
-    highScores.Add((initials, Score));
-    highScores.Sort((a, b) => b.Score.CompareTo(a.Score));
-    if (highScores.Count > 10)
-    {
-        highScores.RemoveAt(10);
-    }
-}
-
-void DisplayHighScores()
-{
-    Console.WriteLine("Top 10 High Scores:");
-    foreach (var (initials, score) in highScores)
-    {
-        Console.WriteLine($"{initials}: {score}");
-    }
-}
-
-
-GameSelection ChooseOption()
-{
-    bool validSelection = false;
-    int selection = 0;
-    while (!validSelection)
-    {
-        Console.WriteLine(
-            "Would you like to:\n\t 1: Play\n\t 2: See the list of high scores\n\t 3: Exit the game?");
-
-        if (int.TryParse(Console.ReadLine(), out selection) && selection >= 1 && selection <= 3)
-            validSelection = true;
-
-    }
-
-    return (GameSelection)selection;
-
-}
-
-enum GameSelection
-{
-    Play = 1, SeeHighScore = 2, Exit = 3
-}
-
-// POS System below
-/*
-void LearningActivity5_2()
-{
-
-}
-
-
-VideoSelection ChooseOption()
-{
-    bool selected = false;
-    int selection = 0;
-    while (!validSelection)
-    {
-        Console.WriteLine(
-            "Would you like to:\n\t 1: Rent a Video\n\t 2: See the list of videos for rent\n\t 3: Exit the program?");
-
-        if (int.TryParse(Console.ReadLine(), out selection) && selection >= 1 && selection <= 3)
-            validSelection = true;
-    }
-
-    return (VideoSelection)selection;
-}
-
-enum VideoSelection
-{
-    Rent = 1, list = 2, exit = 3
-}
-*/
